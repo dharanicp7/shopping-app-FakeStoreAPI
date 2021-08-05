e1638f650be9de2d256d2fa4c7e699ab20492fff
Dharani <dharanicp77@gmail.com>
2021-08-05 11:14:41 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TempComponent } from './component/temp/temp.component';
+import { TemplateComponent } from './component/temp/template.component';
+import { ListComponent } from './component/list/list.component';
+import { ListItemComponent } from './component/list-item/list-item.component';
+import { AddItemComponent } from './component/add-item/add-item.component';
+
+const appRoutes:Routes=[
+  {path:'admin',component:AddItemComponent},
+  {path:'',component:ListComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TempComponent,
+    TemplateComponent,
+    ListComponent,
+    ListItemComponent,
+    AddItemComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [  
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
